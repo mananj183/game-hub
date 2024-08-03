5e7a25b42569210dd331690e7660cc07ecac0dfa
mananj183 <mananj183@gmail.com>
2024-08-04 04:05:49 +0530

    Refactor Game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,32 +12,31 @@ type Props = {
 };
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
+  if (error) return <Text>{error}</Text>;
+
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // create a dummy array with the item number equal to the number of skeletons objects we want to display on the screen
   return (
-    <>
-      {error ?? <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.length === 0 ? (
-          <Text>No Games Found</Text>
-        ) : (
-          data.map((game) => (
-            <GameCardContainer key={game.id}>
-              <GameCard game={game}></GameCard>
-            </GameCardContainer>
-          ))
-        )}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+      {data.length === 0 ? (
+        <Text>No Games Found</Text>
+      ) : (
+        data.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game}></GameCard>
+          </GameCardContainer>
+        ))
+      )}
+    </SimpleGrid>
   );
 };
 
